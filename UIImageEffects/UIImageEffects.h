d412e5b98b6101914ea4197bca257cbc7d3866d7
Son Nguyen <sonnn1992@gmail.com>
2017-03-06 22:05:39 +0700

    no message

---
+++ UIImageEffects/UIImageEffects.h
@@ -0,0 +1,19 @@
+//
+//  UIImageEffects.h
+//  UIImageEffects
+//
+//  Created by Son Nguyen on 3/6/17.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageEffects.
+FOUNDATION_EXPORT double UIImageEffectsVersionNumber;
+
+//! Project version string for UIImageEffects.
+FOUNDATION_EXPORT const unsigned char UIImageEffectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageEffects/PublicHeader.h>
+
+
